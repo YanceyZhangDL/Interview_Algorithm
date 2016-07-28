4d8107478a958aeaba4965f21d66e1ebeb7b047f
张延琪 <zhangyanqi02@meituan.com>
2016-07-29 00:05:23 +0800

    add recursive mothod

--- phonenum_with_words/phonenum_with_words.cpp
+++ phonenum_with_words/phonenum_with_words.cpp
@@ -17,10 +17,8 @@ using namespace std;
  * 给定手机号，输出所有可能在手机键盘上对应的单词组合
 ****************************************************************/
 
-int main()
-{
-	// 数字对应字母索引表
-	char c[10][10] = {
+// 数字对应字母索引表
+char c[10][10] = {
 		
 		"",       // 0
 		"",       // 1
@@ -33,16 +31,41 @@ int main()
 		"TUV",    // 8
 		"WXYZ",   // 9
 	
-	};
-	int total[] = {0, 0, 3, 3, 3, 3, 3, 4, 3, 4};
-	char number[] = {8,8,7,5,3,7,2,9}; // 存电话号码
-	const unsigned int len = strlen(number);
+};
+
+int total[] = {0, 0, 3, 3, 3, 3, 3, 4, 3, 4};// 每一个数字可以代表的字母位数
+
+// 递归方法
+// 对于递归的思路，参考n个for循环的想法
+void RecursiveSearch(int* number, int* answer, int index, int n)
+{
+	if(index == n)
+	{
+		for(int i = 0; i < n; i++)
+			cout << c[number[i]][answer[i]];
+		cout << endl;
+		return;
+	}
+	for(answer[index] = 0;
+		answer[index] < total[number[index]];
+		answer[index]++)
+	{
+		RecursiveSearch(number, answer, index+1, n);	
+	}
+}
+
+int main()
+{
+	int number[] = {8,8,7,5,3,7,2,9}; // 存电话号码
+	const unsigned int len = sizeof(number) / sizeof(int);
 	cout << "len:"<< len << endl;
-	char answer[len]; // 存第几位
-	memset(answer, 0, len*(sizeof(char))); 
+
+	int answer[len]; // 存第几位
+	memset(answer, 0, len*(sizeof(int))); 
 
 	int m = 0;
 	// 非递归方法
+	/*
 	while(true)
 	{
 		for(int i = 0; i < len; i++)
@@ -67,6 +90,9 @@ int main()
 			break;
 
 	}
+	*/
+	// 递归方法
+	RecursiveSearch(number, answer, 0, len);	
 
 	return 0;
 }
