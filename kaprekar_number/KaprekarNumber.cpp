5688b38d44ad3f9c768ea5afa49c326a9ef4821c
张延琪 <yanceyzhang2013@gmail.com>
2016-08-08 13:35:05 +0800

    kaprekar 四位数生成6174

--- kaprekar_number/KaprekarNumber.cpp
+++ kaprekar_number/KaprekarNumber.cpp
@@ -9,47 +9,6 @@
     > Description:
 ****************************************************************/
 
-/*
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int main()
-{
-    unsigned int N;
-    unsigned int a[4];
-    while(cin >> N)
-    {
-        
-        if(N > 10000 || N < 0)
-            continue;
-        
-        while(N != 6174)
-        {
-	    a[0] = N % 10;
-	    a[1] = N / 10 % 10;
-	    a[2] = N / 100 % 10;
-	    a[3] = N / 1000;
-        	
-	    if(a[0]==a[1] && a[1]==a[2] && a[2]==a[3])
-            {	
-                cout << "N - N = 0000" << endl;
-                //cout << endl;
-            	break;
-            }
-            
-            sort(a, a+4);
-            int max = a[3]*1000 + a[2]*100 + a[1]*10 + a[0];
-            int min = a[0]*1000 + a[1]*100 + a[2]*10 + a[3];
-            N = max - min;
-            cout << max << " - " << a[0] << a[1] << a[2] << a[3] << " = " << N << endl;
-        }
-        
-    }
-    return 0; 
-}
-*/
-
 #include <iostream>
 #include <algorithm>
 using namespace std;
